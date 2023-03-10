b5d5dbfef0ac6f05455b5c5291e7c3eb6e35e85f
1989ONCE <susanloveselena@gmail.com>
2023-03-10 21:03:19 +0800

    first version

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CustomAuthController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']); 
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+
+Route::get('/', function () {
+    return view('welcome');
+    // return 'hello world';
+});
+
+Route::get('/login2', [UserController::class, 'index']);
