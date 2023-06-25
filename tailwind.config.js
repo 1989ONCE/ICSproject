1f0f280d7e2f945ce6759cb859485bf1dd3f55de
1989ONCE <susanloveselena@gmail.com>
2023-06-26 05:01:15 +0800

    update modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         "./resources/**/**/**/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -19,5 +20,5 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('postcss-import'), require('tailwindcss'), require('autoprefixer')],
+    plugins: [require('flowbite/plugin'), require('@tailwindcss/forms'), require('postcss-import'), require('tailwindcss'), require('autoprefixer')],
 };
