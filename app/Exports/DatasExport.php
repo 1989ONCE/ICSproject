e06f47afe64fed51897f22b7620fb0074eacd97a
1989ONCE <susanloveselena@gmail.com>
2023-06-28 20:46:35 +0800

    closes #2, feature historical report

--- app/Exports/DatasExport.php
+++ app/Exports/DatasExport.php
@@ -4,14 +4,34 @@ namespace App\Exports;
 
 use App\Models\Datas;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class DatasExport implements FromCollection
+class DatasExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Datas::all();
+        return Datas::all()->map(function($data) {
+            return [
+               'T01_15_ph' => $data->T01_15_ph,
+               'T01_15_temp' => $data->T01_15_temp,
+               'T01_15_ec' => $data->T01_15_ec,
+               'T01_15_cod' => $data->T01_15_cod,
+               'added_on' => $data->added_on
+            ];
+         });
+    }
+
+    public function headings(): array
+    {
+        return [
+            '放流槽ph值',
+            '放流槽水溫',
+            '放流槽導電度',
+            '放流槽COD',
+            '資料添加時間'
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,17 +26,18 @@ Route::get('/', function () { return view('index'); });
 //  ======== function section =============
 // warning management
 Route::get('warning', [WarnController::class, 'index'])->name('warning')->middleware('auth');
-Route::get('/send-warning', [WarnController::class, 'sendWarningNotification']);
+Route::get('/send-warning', [WarnController::class, 'sendWarningNotification'])->middleware('auth');
 
 // realtime data
 Route::get('/realtime', [RealTimeController::class, 'index'])->name('rt');
 Route::post('/realtime', [RealTimeController::class, 'rtdata'])->name('rtdata');
 
-// Route::get('realtime/csv', [RealTimeController::class, 'readCsv'])->name('csv');
-
 // historical chart
 Route::get('chart', [ChartController::class, 'index'])->name('chart')->middleware('auth');
+Route::post('chart', [ChartController::class, 'linechart'])->name('linechart')->middleware('auth');
+Route::post('chart2', [ChartController::class, 'linechart2'])->name('linechart2')->middleware('auth');
 Route::get('chart/export', [ChartController::class, 'export'])->name('export')->middleware('auth');
+Route::get('chart/export2', [ChartController::class, 'export2'])->name('export2')->middleware('auth');
 
 // profile
 Route::middleware('auth')->group(function () {
@@ -54,7 +55,6 @@ Route::get('/email/verify', function () {
 })->middleware('auth')->name('verification.notice');
 Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
     $request->fulfill();
- 
     return redirect('/');
 })->middleware(['auth', 'signed'])->name('verification.verify');
 
