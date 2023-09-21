565fbac58ecf2095c0261aada4e1b5e942dd60e2
1989ONCE <susanloveselena@gmail.com>
2023-09-21 16:33:30 +0800

    exception handler bug test

--- app/Console/Commands/rtCommand.php
+++ app/Console/Commands/rtCommand.php
@@ -37,42 +37,37 @@ class rtCommand extends Command
                 $output = shell_exec($input);
                 $output2 = shell_exec($drug_input);
 
-                if($output){
-                    $output = str_replace( array('[', ']', '\n'), '', $output);
-                    $output = preg_replace( "/\n/", "", $output);
-                    $values = explode(", ", $output);
+                $output = str_replace( array('[', ']', '\n'), '', $output);
+                $output = preg_replace( "/\n/", "", $output);
+                $values = explode(", ", $output);
 
-                    $output2 = str_replace( array('[', ']', '\n'), '', $output2);
-                    $output2 = preg_replace( "/\n/", "", $output2);
-                    $values2 = explode(", ", $output2);
+                $output2 = str_replace( array('[', ']', '\n'), '', $output2);
+                $output2 = preg_replace( "/\n/", "", $output2);
+                $values2 = explode(", ", $output2);
 
-                    $data = new Datas;
+                $data = new Datas;
     
-                    $data->added_on = $timestamp;
+                $data->added_on = $timestamp;
                     
-                    //ph & ss
-                    $data->T01_6_ph = number_format($values[1]/100, 1, '.', ','); // port 101
-                    $data->T01_6_ss = number_format($values[2], 1, '.', ','); // port 102
-                    $data->T01_12_ph = number_format($values[3]/100, 1, '.', ','); // port 104
-                    $data->T01_14_ph = number_format($values[4]/100, 1, '.', ','); // port 105
+                //ph & ss
+                $data->T01_6_ph = number_format($values[1]/100, 1, '.', ','); // port 101
+                $data->T01_6_ss = number_format($values[2], 1, '.', ','); // port 102
+                $data->T01_12_ph = number_format($values[3]/100, 1, '.', ','); // port 104
+                $data->T01_14_ph = number_format($values[4]/100, 1, '.', ','); // port 105
     
-                    // drug record
+                // drug record
 
-		            $data->T01_12_drug1_current = $values2[1];
-	                $data->T01_12_drug2_current = $values2[2];
-                    $data->save();
+		        $data->T01_12_drug1_current = $values2[1];
+	            $data->T01_12_drug2_current = $values2[2];
+                $data->save();
     
-                    // if power return
-                    $first_p = Power::orderBy('onofftime', 'desc')->first();
-                    if($first_p->status == 0) {
-                        $power = new Power;
-                        $power->status = true;
-                        $power->onofftime = date('Y-m-d H:i:s');
-                        $power->save();
-                    }
-                }
-                else{
-                    throw new Exception("設備斷訊");
+                // if power return
+                $first_p = Power::orderBy('onofftime', 'desc')->first();
+                if($first_p->status == 0) {
+                    $power = new Power;
+                    $power->status = true;
+                    $power->onofftime = date('Y-m-d H:i:s');
+                    $power->save();
                 }
         } catch (\Exception $e) {
             // if power has ben cut off
